b8449827340636bf199eee363e1379584e47143b
wilson <wilson@192.168.1.101>
2017-07-12 18:17:15 +0800

    init

---
+++ src/main/java/zdfs/config/RootConfig.java
@@ -0,0 +1,25 @@
+package zdfs.config;
+
+import javax.sql.DataSource;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ComponentScan.Filter;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration
+@ComponentScan(basePackages= {"zdfs"}
+				,excludeFilters= {@Filter(type=FilterType.ANNOTATION,value= {EnableWebMvc.class})})
+public class RootConfig {
+	
+	
+	@Bean
+	public DataSource dataSource() {
+		
+		return null;
+	}
+
+	
+}
---
+++ src/main/java/zdfs/config/WebConfig.java
@@ -0,0 +1,31 @@
+package zdfs.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@EnableWebMvc
+@ComponentScan(basePackages= {"zdfs.web"})
+@Configuration
+public class WebConfig extends WebMvcConfigurerAdapter{
+
+	@Override
+	public void configureViewResolvers(ViewResolverRegistry registry) {
+		// TODO Auto-generated method stub
+		super.configureViewResolvers(registry);
+		registry.viewResolver(new InternalResourceViewResolver());
+	}
+
+	@Override
+	public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+		// TODO Auto-generated method stub
+		super.configureDefaultServletHandling(configurer);
+		configurer.enable();
+	}
+	
+	
+}
